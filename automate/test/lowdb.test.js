b6324215719db60090d9b5b8db8e30f831b4ef2c
Per Wramdemark <per@wramdemark.se>
2025-01-11 16:47:49 +0100

    fix test case

--- automate/test/lowdb.test.js
+++ automate/test/lowdb.test.js
@@ -1,7 +1,18 @@
 import { LowdbBase } from '../database/lowdb.js'
-import { describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
 
 describe('Lets test the lowDb integration', () => {
+  beforeAll(() => {
+    vi.stubEnv('LANGUAGE', 'sv-SE')
+    vi.stubEnv('FOLDER', '/test')
+    vi.stubEnv('THEMOVIEDB_READ_TOKEN', 'g2j390')
+    vi.stubEnv('THEMOVIEDB_ACCOUNT_OBJECT_ID', '842042')
+  })
+
+  afterAll(() => {
+    vi.unstubAllEnvs()
+  })
+
   it('should initialize with an empty collection', async () => {
     const db = new LowdbBase('test.json')
     const data = await db.getCollection()
